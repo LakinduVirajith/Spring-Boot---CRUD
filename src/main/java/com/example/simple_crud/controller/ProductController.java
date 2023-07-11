2ca4de8a57c726629bc1998dd05b4cfb52ed3427
Lakindu <it21066184@my.sliit.lk>
2023-07-11 23:34:57 +0530

    create MySQL database connection and server error handling

--- src/main/java/com/example/simple_crud/controller/ProductController.java
+++ src/main/java/com/example/simple_crud/controller/ProductController.java
@@ -1,7 +1,8 @@
 package com.example.simple_crud.controller;
 
 import com.example.simple_crud.entity.Product;
-import com.example.simple_crud.error.ProductNotFoundException;
+import com.example.simple_crud.error.InternalServerErrorException;
+import com.example.simple_crud.error.NotFoundException;
 import com.example.simple_crud.service.ProductService;
 import jakarta.validation.Valid;
 import org.slf4j.Logger;
@@ -22,44 +23,44 @@ public class ProductController {
     }
 
     @GetMapping("/product")
-    public List<Product> fetchAllProducts() throws ProductNotFoundException {
+    public List<Product> fetchAllProducts() throws NotFoundException, InternalServerErrorException {
         LOGGER.info("fetch all product data");
         return productService.fetchAllProducts();
     }
 
     @PostMapping("/product")
-    public Product saveProduct(@Valid @RequestBody Product product){
+    public Product saveProduct(@Valid @RequestBody Product product) throws InternalServerErrorException {
         LOGGER.info("save product data");
         return productService.saveProduct(product);
     }
 
     @GetMapping("/product/{id}")
-    public Product fetchProduct(@PathVariable("id") Long productId) throws ProductNotFoundException {
+    public Product fetchProduct(@PathVariable("id") Long productId) throws NotFoundException, InternalServerErrorException {
         LOGGER.info("fetch product data using id");
         return productService.fetchProductById(productId);
     }
 
     @DeleteMapping("/product/{id}")
-    public String deleteProduct(@PathVariable("id") Long productId){
+    public String deleteProduct(@PathVariable("id") Long productId) throws NotFoundException, InternalServerErrorException {
         LOGGER.info("delete product data using id");
         productService.deleteProduct(productId);
         return "Product Deleted Successfully";
     }
 
     @PutMapping("/product/{id}")
-    public Product updateProduct(@PathVariable("id") Long productId, @RequestBody Product product){
+    public Product updateProduct(@PathVariable("id") Long productId, @RequestBody Product product) throws InternalServerErrorException {
         LOGGER.info("update product data using id");
         return productService.updateProduct(productId, product);
     }
 
     @GetMapping("/product/search/name/{name}")
-    public Product fetchProductByName(@PathVariable("name") String productName) throws ProductNotFoundException {
+    public Product fetchProductByName(@PathVariable("name") String productName) throws NotFoundException, InternalServerErrorException {
         LOGGER.info("search product data using product name");
         return productService.fetchProductByName(productName);
     }
 
     @GetMapping("product/search/{name}")
-    public List<Product> fetchAllProductByNameIgnoreCase(@PathVariable("name") String productName) throws ProductNotFoundException {
+    public List<Product> fetchAllProductByNameIgnoreCase(@PathVariable("name") String productName) throws NotFoundException, InternalServerErrorException {
         LOGGER.info("search list of products data using product name");
         return productService.searchProductsByName(productName);
     }
--- src/main/java/com/example/simple_crud/service/ProductService.java
+++ src/main/java/com/example/simple_crud/service/ProductService.java
@@ -1,23 +1,24 @@
 package com.example.simple_crud.service;
 
 import com.example.simple_crud.entity.Product;
-import com.example.simple_crud.error.ProductNotFoundException;
+import com.example.simple_crud.error.InternalServerErrorException;
+import com.example.simple_crud.error.NotFoundException;
 
 import java.util.List;
 
 public interface ProductService {
 
-    List<Product> fetchAllProducts() throws ProductNotFoundException;
+    List<Product> fetchAllProducts() throws NotFoundException, InternalServerErrorException;
 
-    Product saveProduct(Product product);
+    Product saveProduct(Product product) throws InternalServerErrorException;
 
-    Product fetchProductById(Long id) throws ProductNotFoundException;
+    Product fetchProductById(Long id) throws NotFoundException, InternalServerErrorException;
 
-    void deleteProduct(Long productId);
+    void deleteProduct(Long productId) throws NotFoundException, InternalServerErrorException;
 
-    Product updateProduct(Long productId, Product product);
+    Product updateProduct(Long productId, Product product) throws InternalServerErrorException;
 
-    Product fetchProductByName(String productName) throws ProductNotFoundException;
+    Product fetchProductByName(String productName) throws NotFoundException, InternalServerErrorException;
 
-    List<Product> searchProductsByName(String productName) throws ProductNotFoundException;
+    List<Product> searchProductsByName(String productName) throws NotFoundException, InternalServerErrorException;
 }
