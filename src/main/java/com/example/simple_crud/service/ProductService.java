9ffdf69ea497c0c21f1bfcfae5366a2a4b6edf9d
Lakindu <it21066184@my.sliit.lk>
2023-07-23 20:13:06 +0530

    fetch using pageable
    fetch using pageable
    readme updated

--- src/main/java/com/example/simple_crud/service/ProductService.java
+++ src/main/java/com/example/simple_crud/service/ProductService.java
@@ -3,6 +3,8 @@ package com.example.simple_crud.service;
 import com.example.simple_crud.entity.Product;
 import com.example.simple_crud.error.InternalServerErrorException;
 import com.example.simple_crud.error.NotFoundException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
@@ -21,4 +23,6 @@ public interface ProductService {
     Product fetchProductByName(String productName) throws NotFoundException, InternalServerErrorException;
 
     List<Product> searchProductsByName(String productName) throws NotFoundException, InternalServerErrorException;
+
+    Page<Product> searchProductByProductDescription(String productDescription, Pageable pageable) throws NotFoundException, InternalServerErrorException;
 }
